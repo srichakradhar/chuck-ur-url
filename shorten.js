3c67c6765f3744f060dd83ec2feb0c813ef6c590
Srichakradhar <srichakra3nsr3@gmail.com>
2016-11-21 12:28:46 +0530

    fixed "mixed content" issue

--- shorten.js
+++ shorten.js
@@ -1,7 +1,7 @@
 $('.btn-shorten').on('click', function(){
 
   $.ajax({
-    url: 'http://cors.io/?http://chuck-url.herokuapp.com/new/' + $("#url-field").val(),
+    url: 'https://chuck-url.herokuapp.com/new/' + $("#url-field").val(),
     type: 'GET',
     dataType: 'JSON',
     success: function(data){
